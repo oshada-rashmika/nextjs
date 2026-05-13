ddb6b97b1287de46a674e5a40912315d84a2b92f
Oshada Rashmika <oshadar.rodrigo@gmail.com>
2026-05-13 19:34:39 +0530

    style: organize auth routes into (auth) group for shared layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const layout = ({children}: {children: React.ReactNode}) => {
   return (
     <div>
-        <h1>Dashboard Layout</h1>
-        {children}
+      <h1>Root Layout</h1>
+      {children}
     </div>
   )
 }
